63af7443c4d81a9929c902cb3374503369311cfa
nhlong1512 <20521569@gm.uit.edu.vn>
2026-06-01 10:13:16 +0700

    fix: blog management

---
+++ source/MoriiCoffee.Domain.Tests/Aggregates/BlogPostAggregateTests.cs
@@ -0,0 +1,68 @@
+using FluentAssertions;
+using MoriiCoffee.Domain.Aggregates.BlogPostAggregate;
+using MoriiCoffee.Domain.Shared.Enums.Blog;
+using Xunit;
+
+namespace MoriiCoffee.Domain.Tests.Aggregates;
+
+public class BlogPostAggregateTests
+{
+    private static BlogPost CreatePost() =>
+        BlogPost.Create(
+            "Post",
+            "post",
+            null,
+            "{\"type\":\"doc\"}",
+            "<p>Post</p>",
+            null,
+            null,
+            null,
+            null,
+            false,
+            0,
+            EBlogPostStatus.Draft);
+
+    [Fact]
+    public void ReplaceCategories_PreservesExistingAssignments()
+    {
+        var post = CreatePost();
+        var categoryId = Guid.NewGuid();
+        post.ReplaceCategories([categoryId]);
+        var originalAssignment = post.BlogPostCategories.Single();
+
+        post.ReplaceCategories([categoryId]);
+
+        post.BlogPostCategories.Should().ContainSingle();
+        post.BlogPostCategories.Single().Should().BeSameAs(originalAssignment);
+    }
+
+    [Fact]
+    public void ReplaceCategories_RemovesMissingAndAddsNewAssignments()
+    {
+        var post = CreatePost();
+        var retainedCategoryId = Guid.NewGuid();
+        var removedCategoryId = Guid.NewGuid();
+        var addedCategoryId = Guid.NewGuid();
+        post.ReplaceCategories([retainedCategoryId, removedCategoryId]);
+        var retainedAssignment = post.BlogPostCategories
+            .Single(link => link.BlogCategoryId == retainedCategoryId);
+
+        post.ReplaceCategories([retainedCategoryId, addedCategoryId]);
+
+        post.BlogPostCategories.Should().HaveCount(2);
+        post.BlogPostCategories.Should().Contain(retainedAssignment);
+        post.BlogPostCategories.Should().NotContain(link => link.BlogCategoryId == removedCategoryId);
+        post.BlogPostCategories.Should().ContainSingle(link => link.BlogCategoryId == addedCategoryId);
+    }
+
+    [Fact]
+    public void ReplaceCategories_DeduplicatesRequestedAssignments()
+    {
+        var post = CreatePost();
+        var categoryId = Guid.NewGuid();
+
+        post.ReplaceCategories([categoryId, categoryId]);
+
+        post.BlogPostCategories.Should().ContainSingle(link => link.BlogCategoryId == categoryId);
+    }
+}
